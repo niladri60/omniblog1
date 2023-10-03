af4917e90eb160f5801945e23a13466858cf4db0
niladri <niladri.stud@gmail.com>
2023-10-03 20:20:28 +0530

    Uploading User data

--- api/index.js
+++ api/index.js
@@ -2,12 +2,20 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
-mongoose.connect(process.env.Mongo_Url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(console.log("Connected to MongoDB")).catch((err) => console.log(err));
+app.use(express.json());
+mongoose
+  .connect(process.env.Mongo_Url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+
+app.use("/api/auth", authRoute);
 
 app.listen(5000, () => {
   console.log(`Backend in running on port 5000`);
