11c6e8bc60f2905de35f93df9cd474443b757845
niladri <niladri.stud@gmail.com>
2023-10-03 21:54:08 +0530

    user Authentication done(Hiding Password)

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 // Register
 router.post("/register", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password,salt);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
@@ -19,6 +19,26 @@ router.post("/register", async (req, res) => {
   }
 });
 
-//Loign
+//Login
+
+router.post("/login", async (req, res) => {
+    try {
+      const user = await User.findOne({ username: req.body.username });
+      if (!user) {
+        return res.status(400).json("Wrong credentials!");
+      }
+  
+      const validated = await bcrypt.compare(req.body.password, user.password);
+      if (!validated) {
+        return res.status(400).json("Wrong credentials!");
+      }
+  
+      const { password, ...others } = user._doc;
+      res.status(200).json(others);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
